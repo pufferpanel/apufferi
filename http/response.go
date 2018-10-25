25ae967fb995ab32895e474b8eea2e3ca30a52eb
Joshua Taylor <jtaylor@fsmb.org>
2018-10-25 13:44:24 -0500

    Add metadata support for paging

--- http/response.go
+++ http/response.go
@@ -1,9 +1,20 @@
 package http
 
 type response struct {
-	Success bool        `json:"success"`
-	Message string      `json:"msg,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Status  int         `json:"-"`
-	Code    Code        `json:"code,omitempty"`
+	Success  bool        `json:"success"`
+	Message  string      `json:"msg,omitempty"`
+	Data     interface{} `json:"data,omitempty"`
+	Status   int         `json:"-"`
+	Code     Code        `json:"code,omitempty"`
+	Metadata *metadata    `json:"metadata,omitempty"`
+}
+
+type metadata struct {
+	Paging *paging `json:"paging"`
+}
+
+type paging struct {
+	Page    uint `json:"page,omitempty"`
+	Size    uint `json:"pageSize,omitempty"`
+	MaxSize uint `json:"maxSize,omitempty"`
 }
--- http/responsebuilder.go
+++ http/responsebuilder.go
@@ -23,10 +23,13 @@ type Builder interface {
 	Fail() Builder
 	Success() Builder
 	WithSuccess(success bool) Builder
+
+	PageInfo(page, pageSize, maxSize uint) Builder
+	WithPageInfo(page, pageSize, maxSize uint) Builder
 }
 
 func Respond(c Context) Builder {
-	return responseBuilder{
+	return &responseBuilder{
 		response: response{
 			Success: true,
 			Status:  200,
@@ -36,11 +39,11 @@ func Respond(c Context) Builder {
 	}
 }
 
-func (rb responseBuilder) Status(status int) Builder {
+func (rb *responseBuilder) Status(status int) Builder {
 	return rb.WithStatus(status)
 }
 
-func (rb responseBuilder) WithStatus(status int) Builder {
+func (rb *responseBuilder) WithStatus(status int) Builder {
 	rb.response.Status = status
 	if status > 299 || status < 200 {
 		return rb.Fail()
@@ -48,46 +51,61 @@ func (rb responseBuilder) WithStatus(status int) Builder {
 	return rb
 }
 
-func (rb responseBuilder) Message(message string) Builder {
+func (rb *responseBuilder) Message(message string) Builder {
 	return rb.WithMessage(message)
 }
 
-func (rb responseBuilder) WithMessage(message string) Builder {
+func (rb *responseBuilder) WithMessage(message string) Builder {
 	rb.response.Message = message
 	return rb
 }
 
-func (rb responseBuilder) Data(data interface{}) Builder {
+func (rb *responseBuilder) Data(data interface{}) Builder {
 	return rb.WithData(data)
 }
 
-func (rb responseBuilder) WithData(data interface{}) Builder {
+func (rb *responseBuilder) WithData(data interface{}) Builder {
 	rb.response.Data = data
 	return rb
 }
 
-func (rb responseBuilder) Code(code Code) Builder {
+func (rb *responseBuilder) Code(code Code) Builder {
 	return rb.WithCode(code)
 }
 
-func (rb responseBuilder) WithCode(code Code) Builder {
+func (rb *responseBuilder) WithCode(code Code) Builder {
 	rb.response.Code = code
 	return rb
 }
 
-func (rb responseBuilder) Fail() Builder {
+func (rb *responseBuilder) Fail() Builder {
 	return rb.WithSuccess(false)
 }
 
-func (rb responseBuilder) Success() Builder {
+func (rb *responseBuilder) Success() Builder {
 	return rb.WithSuccess(true)
 }
 
-func (rb responseBuilder) WithSuccess(success bool) Builder {
+func (rb *responseBuilder) WithSuccess(success bool) Builder {
 	rb.response.Success = success
 	return rb
 }
 
-func (rb responseBuilder) Send() {
+func (rb *responseBuilder) Send() {
 	rb.context.JSON(rb.response.Status, rb.response)
+}
+
+func (rb *responseBuilder) PageInfo(page, pageSize, maxSize uint) Builder {
+	return rb.WithPageInfo(page, pageSize, maxSize)
+}
+
+func (rb *responseBuilder) WithPageInfo(page, pageSize, maxSize uint) Builder {
+
+	rb.response.Metadata = &metadata{
+		Paging: &paging{
+			Page:    page,
+			Size:    pageSize,
+			MaxSize: maxSize,
+		}}
+	return rb
 }
