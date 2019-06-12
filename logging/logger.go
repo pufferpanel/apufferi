950206c075c618bad3f66edd6fd42059f21b631b
Joshua Taylor <lordralex@gmail.com>
2019-06-12 15:09:27 -0500

    Have logger not use routine for feeding data to writers

--- logging/logger.go
+++ logging/logger.go
@@ -56,10 +56,7 @@ func init() {
 
 	go func() {
 		for {
-			select {
-			case msg := <-input:
-				runLogMessage(msg)
-			}
+			runLogMessage(<-input)
 		}
 	}()
 }
@@ -135,6 +132,7 @@ func Log(lvl *Level, msg string, data ...interface{}) {
 	wg.Add(1)
 	select {
 	case input <- logMsg:
+		break
 	default:
 		wg.Done()
 	}
@@ -163,9 +161,7 @@ func logString(lvl *Level, output string) {
 	for _, v := range writers {
 		//log to this writer messages which are either the same level or higher, but not over the max
 		if lvl.GetScale() >= v.level.GetScale() && (v.ignore == nil || lvl.GetScale() < v.ignore.GetScale()) {
-			go func(w io.Writer, o string) {
-				_, _ = fmt.Fprint(w, o)
-			}(v.writer, output)
+			_, _ = fmt.Fprint(v.writer, output)
 		}
 	}
 	wg.Done()
